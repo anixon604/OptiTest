7086bf0fc6b2c4fa0942ccc0909c8b7bff9bb289
Anthony Nixon <anixon604@gmail.com>
2013-03-18 12:45:11 -0700

    The skeleton of student Class as shown on Cacoo

--- Student.py
+++ Student.py
@@ -1,1 +1,15 @@
 # Student Class
+class Student(object):
+    #define class to simulate a simple calculator
+    def __init__ (self, name, age, level):
+        #initialize properties
+        self.name = ""
+        self.age = 0
+        self.abilityLevel = 0
+        self.testResults = []
+    def getStundentLevel(self):
+        #put AI here to return converge level of student based on tests
+        return 0
+    def probAnsFunc(self):
+        #utilizes self.abilityLevel to return Prob function of the brain
+        self.current += amount
