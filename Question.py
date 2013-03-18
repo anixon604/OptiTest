e52963487a95a559d507b16d37dafce333bedb3e
Jennifer Waldern <jfwaldern@gmail.com>
2013-03-18 16:53:05 -0700

    Question working now
    Produces list of random question levels (where num ques = 30)

--- Question.py
+++ Question.py
@@ -11,14 +11,24 @@ class Question:
     # Question Features = level only for now
     num_feat = 1
     
-    def __init__(self):
-        self.question = np.zeros(Question.num_feat)
-        self.question = self.get_Features()
-
-    def get_Features(self):
+    def __init__(self, q_num):
         
-        #generate level as [level]
+        # initialize list of length num_feat to 0 
+        self.question = [0] * Question.num_feat
+        
+        # assign a random level
+        # only one feature (level) at question[0] for now
         self.question[0] = random.randint(1,10)
+    
+    def __repr__(self):
+        return ''.join(str(e) for e in self.question)
+    
+    def __iter__(self):
+    	return self.question
+    
+    def __str__(self):
+        return ''.join(str(e) for e in self.question)
+
         
         
         
