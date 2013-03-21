7795354bf11c663f99e390d574a278b7d0ac2d3b
Jennifer Waldern <jfwaldern@gmail.com>
2013-03-21 14:31:46 -0700

    Assesment driver has basic functionality

--- ADriver.py
+++ ADriver.py
@@ -4,6 +4,27 @@ from Test import * #get classes Test
 #
 
 # Create an array of students with varying ability levels
+def test_Drive_Baseline():
+    
+    # Create 30 students 
+    stud_List = gen_studentList(30)
+
+    # Test the students
+    # Create a Test( num_q, list of students)
+    test1 = Test(30, stud_List)
+
+
+# generates a list of students of length len
+def gen_studentList(length)
+
+    student[0]*0
+
+    for i in range(length):
+        
+        # Student ( ID, age, level)
+        student[i] = Student(i, random.randint(10, 99),random.randint(1,10))
+
+    return student 
 
 
 
