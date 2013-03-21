bafc1ead6508e1859c1462e20dfb73c05146a6b1
jungkumseok <jungkumseok@gmail.com>
2013-03-21 16:10:04 -0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random as rand
 from QuestionList import QuestionList
 
 # Test Class
@@ -29,7 +30,7 @@ class Test:
     def getQListBetween(self,num_q,min,max):
         self.qListRasch = QuestionList(num_q,min,max)
 	
-    #This function takes care of everything
+    #This function takes care of everything. Just go TEST_INSTANCE_NAME.start() to start the testing process
     #It iterates through a list of students,
     #calling the testStudent function for each student	
     def start(self):
@@ -38,14 +39,39 @@ class Test:
             print("Student #"+str(i)+":  ")
         print("Test Finished")
     
+	#This is a function to test ONE student with the given QuestionList
     def testStudent(self, qList, student):
+        prob = self.probDistribution(student)
+        print prob
         for i in range(len(self.qList)):
-            self.askQuestion(student, qList[i])
-        return 0
-    def askQuestion(self, student, question):
-        print("Student "+str(student)+"  answers Question level "+str(question))
+            self.askQuestion(student, prob, qList[i])
+
+    #This is a function to test ONE student with ONE question from QuestionList
+	#In this function, result is updated.
+	#Whether the student gets it right or wrong depends on the random variable
+	#corr compared against the probability distribution function of the given student.
+    def askQuestion(self, student, prob, question):
+        corr = rand.random()
+        if corr < prob[question.question[0]-1]:
+            print("Student "+str(student)+"  answers Question level "+str(question)+" and is CORRECT")
+        else:
+            print("Student "+str(student)+"  answers Question level "+str(question)+" and is WRONG")
+
+	#Will return results for use
     def getResult(self):
 	    return 0
+    
+	#function to generate a probability distribution function
+    def probDistribution(self,level):
+        prob0 = 1
+        prob = np.zeros(10)
+        for i in range(level):
+            prob[i] = 1
+        for i in range(level,10):
+            prob[i] = prob0 - 0.1
+            prob0 = prob0 - 0.1
+        return prob
+	
 newTest = Test(20,[1,2,3,4,5,6,7,8,9,10])
 newTest.start()
 raw_input("Test successful")
