ea777755bb3bc200e0c9fd8d79d3534aa9219f21
Anthony Nixon <anixon604@gmail.com>
2013-03-21 13:11:37 -0700

    Adding ADriver class. Simplified Student Class

---
+++ ADriver.py
@@ -0,0 +1,11 @@
+from Student import * #get classes Student
+from Test import * #get classes Test
+
+# Create an array of students with varying ability levels
+
+
+
+# Instantiate a TEST object with an array of Students
+
+
+
--- Student.py
+++ Student.py
@@ -1,6 +1,4 @@
 # Student Class
-import math
-import sqlite3
 class Student(object):
     #define class to simulate a simple calculator
     def __init__ (self, name, age, level):
@@ -9,12 +7,3 @@ class Student(object):
         self.age = age
         self.abilityLevel = level
         self.testResults = []
-    def getStundentLevel(self):
-        #put AI here to return converge level of student based on tests
-        return 0
-    def probAnsFunc(self, qDifficulty):
-        #utilizes self.abilityLevel to return Prob function of the brain
-        # Rasch function Pr{ Xni = 1 } = (e^(Bn-qi))/(1+e^(Bn-qi))
-        self.prSuccess = math.e ** (self.abilityLevel - qDifficulty) # numerator
-        self.prSuccess = self.prSuccess / (1 + math.e ** (self.abilityLevel - qDifficulty)) # divide by denominator of function
-        return self.prSuccess
