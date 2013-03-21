03043975e1b0f32a9763285b24fd378afc592391
Jennifer Waldern <jfwaldern@gmail.com>
2013-03-21 14:16:36 -0700

    Questions Cleaned up w/comments

--- Question.py
+++ Question.py
@@ -12,22 +12,38 @@ import numpy as np
 # either at random between two indexes, lev_low and lev_high if they are specified
 # else at random between 1 and 10 
 class Question:
+
+    num_feat = 1 # Question Features = level only for now
     
-    # Question Features = level only for now
-    num_feat = 1
-    
+    # Must initialize a Question with (low_lev range, high_lev range)
+    # Returns a question in the form [1,2,3,4,5,6,1,2,3,4,5,.....,2,3]
+    # cooresponding to a list of 'Questions'
     def __init__(self, lev_low = None, lev_high= None):
-        
-        # initialize list of length num_feat all to 0 (ie [0,0,0,0....0])
+
         self.question = [0] * Question.num_feat
-        
-        # assign a random level
-        # only one feature (level) at question[0] for now
         self.question[0] = self.get_Level(lev_low, lev_high)
-        print self.question[0]
-    
+        
+        ### NOT YET USED ####
         self.result = self.get_Result()
     
+    # Return a Question Level between lev_low and lev_high
+    # Input: low_lev; D:{x| 1 < x < high_lev}
+    #        high_lev; D:{x| low_lev < x < 10}
+    # Output: A random integer between lev_low and lev_high  
+    def get_Level(self, lev_low, lev_high):
+        
+        if( (lev_high <= 10) and (lev_low >=0)):
+            return random.randint(lev_low, lev_high)
+            
+        else:
+            return random.randint(1,10)
+
+############### HOW WE DETERMINE RESULT NEEDS TO BE DONE STILL ################
+    def get_Result(self):
+        return 0
+
+# Iteration, String and Representation Functions
+# Defined for testing (printing and iterating) purposes
     def __repr__(self):
         return ''.join(str(e) for e in self.question)
     
@@ -36,17 +52,7 @@ class Question:
     
     def __str__(self):
         return ''.join(str(e) for e in self.question)
-    
-    def get_Level(self, lev_low, lev_high):
-        if( (lev_high <= 10) and (lev_low >=0)):
-            return random.randint(lev_low, lev_high)
-            
-        else:
-            return random.randint(1,10)
 
-############### HOW WE DETERMINE RESULT NEEDS TO BE DONE STILL (must ################
-    def get_Result(self):
-        return 0
 
         
         
--- QuestionList.py
+++ QuestionList.py
@@ -9,37 +9,41 @@ from Question import Question
 #            #
 ##############
 class QuestionList:
-    #Global/Class Vars for each instance
-    # 30 Questions per Question List for now 
-    
-    #def __init__(self, num_q):
-    #   self.questions = self.get_Questions(num_q, 1, 10)
-    
+
+    # Must initialize a QuestionList with (num_q, low_lev range, high_lev range)
+    # Returns a list in the form [1,2,3,4,5,6,1,2,3,4,5,.....,2,3]
+    # cooresponding to a list of 'Questions' 
     def __init__(self, num_q, low_lev, high_lev):
         self.questions = self.get_Questions(num_q, low_lev, high_lev)
     
-    # returns a question with all necessary
-    # parameters (level)
+    # Return a list of Questions
+    # Input: num_q; D:{x| 1 < x < ques_num}
+    #        low_lev; D:{x| 1 < x < high_lev}
+    #        high_lev; D:{x| low_lev < x < 10}
+    # Output: A list of Question objects of the form [ Ques1, Ques2, Ques3 ]
     def get_Questions(self, num_q, low_lev, high_lev):
         
-        
-        # create 1D array size [0,0,0,...0] n = 30 feature initialized to zeros 
-        #questions = np.zeros(num_q)
         questions = [0] * 0
         for i in range(num_q):
             questions.append(Question(low_lev, high_lev))
     
-        print questions
+        #print questions
         return questions
 
-            #def get_CalculatedQuestions(self):
-            #return 0
-
+    # Return a specific question level
+    # Input: ques_number D:{x| 1 < x < ques_num} 
+    # Output: integer question value 
     def get_Question(self, ques_number):
         return questions[ques_number + 1]
 
+    # Return a the next question level
+    # Input: ques_number D:{x| 1 < x < ques_num}
+    # Output: next integer question value
+    def get_NextQuestion(self, ques_number):
+        return questions[ques_number + 2]
+
 # Test run
-listOfQ = QuestionList(30,1,10)
+#listOfQ = QuestionList(30,1,10)
 
         
         
