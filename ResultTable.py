641554f5b7353c3bd7899986f2f6d2314a4136cb
Anthony Nixon <anixon604@gmail.com>
2013-03-21 15:09:45 -0700

    result table creation function added

--- ResultTable.py
+++ ResultTable.py
@@ -1,5 +1,19 @@
-class ResultTable:
+# Utility file ResultTable.py
+
+# import and create connections to database
+import sqlite3
+conn = sqlite3.connect("testResults.db") # or use :memory: to put it in RAM
+cursor = conn.cursor()
+
+# create table entries for each student
+# param: an array of Students, number of levels in test
+def createResultEntries(students, levels):
+    for i in students:
+        for j in range(1,levels+1):
+            cursor.execute("INSERT INTO testResults VALUES(%s, %s, 0)", (i.studentID,j))
 
 # updates counts on resultTable
-
+# param: Student student, integer level, Bool correct
+def updateCount(student, level, correct):
     
+    print(test)
--- StudentTest.py
+++ StudentTest.py
@@ -1,6 +1,8 @@
 # Tester class for Student
 from Student import * #get classes Student
+from ResultTable import *
+
 myStudent = Student("bob",20,11) # create a student object
-print(myStudent.name)
+print(myStudent.studentID)
 print(myStudent.age)
 print(myStudent.abilityLevel)
