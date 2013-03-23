17a04667eb9e16ef1180115c2b64f91f073299e3
jungkumseok <jungkumseok@gmail.com>
2013-03-23 14:58:13 -0700

    Update KZ_ADriver.py

--- KZ_ADriver.py
+++ KZ_ADriver.py
@@ -5,24 +5,28 @@ import random
 #
 class ADrive: 
 # Create an array of students with varying ability levels
-	def __init__(self,num_s):
-		self.students = self.gen_students(num_s)    
+    def __init__(self,num_s):
+        students = self.gen_students(num_s)
+        newTest = Test(500, students)
+        newTest.start()
+        newTest.printResult()
 
+    def gen_students(self, num_s):
 
-	def gen_students(num_s):
-
-		students = [0]*0
+        students = [0]*num_s
 	
-		for i in range(num_s):
-			id = random.randint(1,10)
-			age = random.randint(15,25)
-			students[i] = Student(i, id,age, none)
-			print(i)
-		return students
+        for i in range(num_s):
+            age = random.randint(15,25)
+            level = random.randint(1,10)
+            students[i] = Student(i, age, level)
+			#print(i)
+        return students
 			
-	def get_student(self, stu_num):
-		return students[stu_number + 1]
+    def get_student(self, stu_num):
+        return students[stu_num]
 
+tester = ADrive(10)
+raw_input(">>hit Enter")
 
 
 # Instantiate a TEST object with an array of Students
