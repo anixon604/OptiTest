c56df768cdf90cc16f364c0ee763b12ddff83f2a
jungkumseok <jungkumseok@gmail.com>
2013-04-03 23:41:35 -0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -15,7 +15,6 @@ class Test:
     qList = 0
     qListRasch = 0
     results = []
-    error = 0.0
 	
     def __init__ (self, num_q, students):	    
         #getQList gets a standard QuestionList with num_q questions
@@ -23,7 +22,7 @@ class Test:
         self.students = students
         self.getQList(num_q)
         self.results = [0]*len(self.students)
-        rt.createResultEntries(self.students,10)
+        #rt.createResultEntries(self.students,10)
         for i in range(len(self.students)):
             self.results[i] = [0,0,0]
             self.results[i][0] = [0]*num_q
@@ -45,14 +44,51 @@ class Test:
         for i in range(len(self.students)):
             self.results[i][2] = self.testStudent(self.qList, self.students[i], i)
         #print("Test Finished")
+    def start_opt(self):
+        for i in range(len(self.students)):
+            self.results[i][2] = self.testStudent_opt(self.students[i], i)
     
 	#This is a function to test ONE student with the given QuestionList
     def testStudent(self, qList, student, index):
         #prob = self.probDistribution(student)
         scoreSum = 0
         for i in range(len(self.qList)):
-            scoreSum = scoreSum + self.askQuestion(student, qList[i].question[0], index)
+            scoreSum += self.askQuestion(student, qList[i].question[0], index)
         return scoreSum
+
+    def testStudent_opt(self, student, index):
+        self.setupTable(index, 5, 5)
+        scoreSum = 0
+		
+        explore = 0.5
+        greedy = 0.8
+        exp_stage = int(explore*self.num_q)
+		
+        for i in range(self.num_q):
+            cur_estimate = self.estimateLevel(index)
+            question = int(rand.random() * 10)
+            if i < exp_stage:
+                scoreSum += self.askQuestion(student, question, index)
+            else:
+                exploit = rand.random()
+                if exploit < greedy:
+                    scoreSum += self.askQuestion(student, int(cur_estimate), index)
+                else:
+                    scoreSum += self.askQuestion(student, question, index)
+            
+			
+			
+
+        for i in range(exp_stage, self.num_q):
+            cur_estimate = self.estimateLevel(index)
+            question = int(rand.random() * 10)
+        return scoreSum
+
+    def setupTable(self, index, level, num_q):
+        for i in range(10):
+            self.results[index][0][i] = np.round(num_q*0.95/(1+np.exp((i+1)-level-1.3333)))
+            self.results[index][1][i] = num_q
+
     #This is a function to test ONE student with ONE question from QuestionList
 	#In this function, result is updated.
 	#Whether the student gets it right or wrong depends on the random variable
@@ -67,7 +103,7 @@ class Test:
             self.results[index][0][question-1] += 1
             score = 1
         self.results[index][1][question-1] += 1
-        rt.updateCount(student, question, score)
+        #rt.updateCount(student, question, score)
         #else:
             #print("Level "+str(question)+" | X")
         return score
@@ -94,8 +130,7 @@ class Test:
         print("------------------------------------------------")
         for i in range(len(self.students)):
             print("  "+str(i)+"\t| "+str(self.students[i].abilityLevel)+"\t\t| "+str(self.estimateLevel(i))+"\t\t| "+str(np.abs(self.students[i].abilityLevel - self.estimateLevel(i))))
-            self.error += np.abs(self.students[i].abilityLevel - self.estimateLevel(i))
-        print("Avg Error: " + str(self.error/len(self.students)))
+
 
     #Student abilityLevel diagnosis
     def estimateLevel(self,sindex):
@@ -109,7 +144,7 @@ class Test:
         level += (self.results[sindex][0][7]*8)/self.results[sindex][1][7]
         level += (self.results[sindex][0][8]*9)/self.results[sindex][1][8]
         level += (self.results[sindex][0][9]*10)/self.results[sindex][1][9]
-        level = (level*10.0)/55.0 + 1.5
+        level = level*10/55 + 1.5
         return level
 
 	
